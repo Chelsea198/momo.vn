87e20013d8fff07f0aa8f86e1466fc262d1a83c8
Chelsea198 <Phantrongba1998@gmail.com>
2021-08-25 15:09:45 +0700

    Chane file lib

--- lib/momo_vn.dart
+++ lib/momo_vn.dart
@@ -1,12 +1,12 @@
 import 'dart:io' show Platform;
 import 'package:flutter/services.dart';
 import 'package:eventify/eventify.dart';
-import 'dart:convert' as convert;
 
 class MomoVn {
   // Response codes from platform
   static const _CODE_PAYMENT_SUCCESS = 0; //User xác nhận thanh toán thành công
-  static const _CODE_PAYMENT_TIMEOUT = 5; // Hết thời gian thực hiện giao dịch (Timeout transaction)
+  static const _CODE_PAYMENT_TIMEOUT =
+  5; // Hết thời gian thực hiện giao dịch (Timeout transaction)
   static const _CODE_PAYMENT_CANCEL = 6; // Người dùng huỷ thanh toán
   static const _CODE_PAYMENT_ERROR = 7; // Lỗi Không xác định
 
@@ -16,7 +16,7 @@ class MomoVn {
 
   static const MethodChannel _channel = const MethodChannel('momo_vn');
 
-  late EventEmitter _eventEmitter;
+  EventEmitter _eventEmitter;
 
   MomoVn() {
     _eventEmitter = new EventEmitter();
@@ -25,12 +25,18 @@ class MomoVn {
   /// Opens checkout
   void open(MomoPaymentInfo options) async {
     PaymentResponse validationResult = _validateOptions(options);
-    if (!validationResult.isSuccess!) {
+    if (!validationResult.isSuccess) {
       _handleResult({'type': _CODE_PAYMENT_ERROR, 'data': validationResult});
       return;
     }
-    var response = await _channel.invokeMethod('open', options.toJson());
-    _handleResult({'data': response, 'type': response['status']});
+    try {
+      await _channel.invokeMethod('open', options.toJson()).then((response) {
+        _handleResult({'data': response, 'type': response['status']});
+      });
+    }on PlatformException catch (e) {
+      print(e);
+    }
+
   }
 
   /// Handles checkout response from platform
@@ -48,7 +54,8 @@ class MomoVn {
         break;
       default:
         eventName = EVENT_PAYMENT_ERROR;
-        payload = PaymentResponse(false, _CODE_PAYMENT_ERROR, '', '', 'Lỗi không xác định', '', '');
+        payload = PaymentResponse(
+            false, _CODE_PAYMENT_ERROR, '', '', 'Lỗi không xác định', '', '');
     }
     _eventEmitter.emit(eventName, null, payload);
   }
@@ -68,105 +75,114 @@ class MomoVn {
   static PaymentResponse _validateOptions(MomoPaymentInfo options) {
     bool error = false;
     String mes = '';
-    if (options.merchantCode == null) {
-      mes = 'merchantcode is required. Please check if key is present in options.';
-      error = true;
+    if (options.merchantcode == '' || options.merchantcode == null) {
+      mes =
+      'merchantcode is required. Please check if key is present in options.';
     }
-    if (options.merchantName.isEmpty) {
-      mes = 'merchantcode is required. Please check if key is present in options.';
-      error = true;
+    if (options.merchantname == null || options.merchantname.isEmpty) {
+      mes =
+      'merchantcode is required. Please check if key is present in options.';
     }
-    if (options.partner.isEmpty) {
-      mes = 'merchantcode is required. Please check if key is present in options.';
-      error = true;
+    if (options.partner == null || options.partner.isEmpty) {
+      mes =
+      'merchantcode is required. Please check if key is present in options.';
     }
-    if (Platform.isIOS && (options.appScheme.isEmpty)) {
+    if (Platform.isIOS &&
+        (options.appScheme == null || options.appScheme.isEmpty)) {
       mes = 'appScheme is required. Please check if key is present in options.';
-      error = true;
     }
-    if (options.amount < 0) {
+    if (options.amount == null || options.amount < 0) {
       mes = 'amount is required. Please check if key is present in options.';
-      error = true;
     }
-    if (options.description == null || options.description!.isEmpty) {
-      mes = 'description is required. Please check if key is present in options.';
-      error = true;
+    if (options.description == null || options.description.isEmpty) {
+      mes =
+      'description is required. Please check if key is present in options.';
     }
-    return error ? PaymentResponse(false, _CODE_PAYMENT_ERROR, '', '', mes, '', '') : PaymentResponse(true, _CODE_PAYMENT_SUCCESS, '', '', '', '', '');
+    if (error) {
+      return PaymentResponse(false, 7, '', '', mes, '', '');
+    }
+    return PaymentResponse(true, 0, '', '', '', '', '');
   }
 }
 
 class PaymentResponse {
-  bool? isSuccess;
+  bool isSuccess;
   int status;
-  String? token;
-  String? phoneNumber;
-  String? data;
-  String? message;
-  String? extra;
+  String token;
+  String phonenumber;
+  String data;
+  String message;
+  String extra;
 
-  PaymentResponse(this.isSuccess, this.status, this.token, this.phoneNumber, this.message, this.data, this.extra);
+  PaymentResponse(this.isSuccess, this.status, this.token, this.phonenumber,
+      this.message, this.data, this.extra);
 
   static PaymentResponse fromMap(Map<dynamic, dynamic> map) {
-    bool? isSuccess = map["isSuccess"];
-    int status = int.parse(map['status'].toString());
-    String? token = map["token"];
-    String? phoneNumber = map["phoneNumber"];
-    String? data = map["data"];
-    String? message = map["message"];
-    String? extra = "";
-    extra = map["extra"];
-    return new PaymentResponse(isSuccess, status, token, phoneNumber, data, message, extra);
+    return new PaymentResponse(
+        map['isSuccess'],
+        map['status'] != null ? int.parse(map['status'].toString()) : null,
+        map['token'] != null ? map['token'] as String : null,
+        map['phonenumber'] != null ? map['phonenumber'] as String : null,
+        map['message'] != null ? map['message'] as String : null,
+        map['data'] != null ? map['data'] as String : null,
+        map['extra'] != null ? map['extra'] as String : null);
   }
 }
 
 class MomoPaymentInfo {
   String partner;
   String appScheme;
-  String merchantName;
-  String merchantCode;
-  String partnerCode;
-  String merchantNameLabel;
+  String merchantname;
+  String merchantcode;
+  String merchantnamelabel;
 
-  int amount;
-  int fee;
-  String? description;
-  String? extra;
-  String? username;
+  double amount;
+  double fee;
+  String description;
+  String extra;
+  String username;
   String orderId;
   String orderLabel;
 
   bool isTestMode;
 
   MomoPaymentInfo({
-    required this.appScheme,
-    required this.merchantName,
-    required this.merchantCode,
-    required this.partnerCode,
-    required this.amount,
-    required this.orderId,
-    required this.orderLabel,
-    required this.partner,
-    required this.merchantNameLabel,
-    required this.fee,
+    this.appScheme,
+    this.merchantname,
+    this.merchantcode,
+    this.amount,
+    this.orderId,
+    this.orderLabel,
+    this.partner,
+    this.merchantnamelabel,
+    this.fee,
     this.description,
     this.username,
     this.extra,
     this.isTestMode = false,
-  });
+  }) : assert(merchantname != null &&
+      merchantname.isNotEmpty &&
+      merchantcode != null &&
+      merchantcode.isNotEmpty &&
+      amount != null &&
+      amount > 0 &&
+      orderId != null &&
+      orderId.isNotEmpty &&
+      orderLabel != null &&
+      orderLabel.isNotEmpty &&
+      partner != null &&
+      partner.isNotEmpty);
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {
-      "merchantname": this.merchantName,
-      "merchantcode": this.merchantCode,
-      "partnercode": this.partnerCode,
+      "merchantname": this.merchantname,
+      "merchantcode": this.merchantcode,
       "amount": this.amount,
-      "orderid": this.orderId,
-      "orderlabel": this.orderLabel,
+      "orderId": this.orderId,
+      "orderLabel": this.orderLabel,
       "partner": this.partner,
-      "fee": this.fee,
-      "isTestMode": isTestMode,
-      "merchantnamelabel": merchantNameLabel
+      "fee": this.fee ?? 0,
+      "isTestMode": isTestMode ?? true,
     };
     if (Platform.isIOS) {
       json["appScheme"] = appScheme;
@@ -176,6 +192,9 @@ class MomoPaymentInfo {
     }
     if (username != null) {
       json["username"] = username;
+    }
+    if (merchantnamelabel != null) {
+      json["merchantnamelabel"] = merchantnamelabel;
     }
     if (extra != null) {
       json["extra"] = extra;
